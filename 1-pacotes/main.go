f5b6b8904acb953f5a771260ef442e939c335fb7
Lucas Volpato <lmaciel916@gmail.com>
2024-06-03 18:06:12 +0000

    usando pacotes externos

--- 1-pacotes/main.go
+++ 1-pacotes/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo da função main")
 	auxiliar.Escrever()
+	err := checkmail.ValidateFormat("email")
+	fmt.Println(err)
 }
