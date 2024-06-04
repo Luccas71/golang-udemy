42249cb7f6014622ef33e4610528b8f6e720e76b
Lucas Volpato <lmaciel916@gmail.com>
2024-06-04 18:42:59 -0300

    switch

--- 12-switch/main.go
+++ 12-switch/main.go
@@ -1,5 +1,34 @@
 package main
 
-func diaDaSemana() {}
+import "fmt"
 
-func main() {}
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+		// fallthrough => avança direto para a proxima condição
+	case 2:
+		return "Segunda"
+	case 3:
+		return "Terça"
+	case 4:
+		return "Quarta"
+	case 5:
+		return "Quinta"
+	case 6:
+		return "Sexta"
+	case 7:
+		return "Sabado"
+	default:
+		return "Numero Invalido"
+	}
+
+	//passando a condição dentro do case:
+	// case numero == 1: todo
+
+	// Go NÃO exige a utilização do break no switch
+}
+
+func main() {
+	fmt.Println(diaDaSemana(69))
+}
