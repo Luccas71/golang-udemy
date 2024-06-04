1cbf31beb2c9fd383042439f7f334a1585087347
Lucas Volpato <lmaciel916@gmail.com>
2024-06-04 19:05:02 +0000

    arrays e slices 2

--- 9-arrays-slices/main.go
+++ 9-arrays-slices/main.go
@@ -17,4 +17,20 @@ func main() {
 	array3 := [...]int{1, 2, 3}
 	fmt.Println(array3)
 	// array3[4] = 12 nao é permitido
+
+	//SLICE
+	//declarando um slice
+	slice1 := make([]int, 3, 5)
+	fmt.Println("Antes de estourar a capacidade")
+	fmt.Println(slice1)
+	fmt.Println(len(slice1)) //tamanho
+	fmt.Println(cap(slice1)) //capacidade
+
+	//capacidade do slice é dinamica. Sempre que sua tamanho atinge a capacidade, a capacidade é dobrada
+
+	slice1 = append(slice1, 2, 3, 5)
+	fmt.Println("Apos estourar a capacidade")
+	fmt.Println(slice1)
+	fmt.Println(len(slice1)) //tamanho
+	fmt.Println(cap(slice1)) //capacidade
 }
