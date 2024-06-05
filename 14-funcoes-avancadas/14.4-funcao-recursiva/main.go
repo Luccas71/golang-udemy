ccb4be5f7f5f652deb70112da5c4b04a34d03328
Lucas Volpato <lmaciel916@gmail.com>
2024-06-05 17:30:08 +0000

    funcao recursiva

--- 14-funcoes-avancadas/14.4-funcao-recursiva/main.go
+++ 14-funcoes-avancadas/14.4-funcao-recursiva/main.go
@@ -15,7 +15,23 @@ func fibonacci(posicao int) int {
 	return fibonacci(posicao-2) + fibonacci(posicao-1)
 	// 1 1 2 3 5 8 13 21 34 55
 }
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+
+	// fatorial(3) chama fatorial(2).
+	// fatorial(2) chama fatorial(1).
+	// fatorial(1) chama fatorial(0).
+	// fatorial(0) retorna 1 (caso base).
+	// fatorial(1) retorna 1 * 1 = 1.
+	// fatorial(2) retorna 2 * 1 = 2.
+	// fatorial(3) retorna 3 * 2 = 6.
+}
 func main() {
-	posicao := fibonacci(7)
-	fmt.Println(posicao)
+	// posicao := fibonacci(7)
+	fac := factorial(3)
+	fmt.Println(fac)
 }
