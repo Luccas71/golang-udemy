110cc5044b21e8006e82fba2f30d80cbd69ea087
Lucas Volpato <lmaciel916@gmail.com>
2024-06-05 19:14:36 +0000

    closure

---
+++ 14-funcoes-avancadas/14.7-closure/main.go
@@ -0,0 +1,20 @@
+package main
+
+// mantem as variaveis criadas ma funçao permitindo que sejam usadas
+import "fmt"
+
+func closure() func() {
+	texto := "Dentro da funcao closure"
+
+	funcao := func() {
+		fmt.Println(texto)
+	}
+	return funcao
+}
+func main() {
+	texto := "Alo"
+	fmt.Println(texto)
+
+	funcClosure := closure()
+	funcClosure()
+}
