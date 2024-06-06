73983c70e35bca487e003d8dee7e9839d42af31a
Lucas Volpato <lmaciel916@gmail.com>
2024-06-06 19:40:19 +0000

    buscar servidores

--- 17-app-cli/app/app.go
+++ 17-app-cli/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net"
 
@@ -11,20 +12,28 @@ func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IP e nomes de servidores na internet"
+	//separar flags numa variavel, pois sera reutilizada
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name: "host",
+			//valor padrao se nada for passado
+			Value: "google.com.br",
+		},
+	}
 	app.Commands = []cli.Command{
 		{
 			//comando de buscar ip
 			Name:  "ip",
 			Usage: "Buscar IPs de endereços na internt",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name: "host",
-					//valor padrao se nada for passado
-					Value: "devbook.com.;br",
-				},
-			},
+			Flags: flags,
 			// recebe uma funcao
 			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "buscar servidor onde site está hospedado",
+			Flags:  flags,
+			Action: buscarServidores,
 		},
 	}
 
@@ -40,6 +49,18 @@ func buscarIps(c *cli.Context) {
 		log.Fatal(err)
 	}
 	for _, ip := range ips {
-		println(ip)
+		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }
