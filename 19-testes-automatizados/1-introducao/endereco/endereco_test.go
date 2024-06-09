0fda0a89828d600d3304bb677806664c7122dade
Lucas Volpato <lmaciel916@gmail.com>
2024-06-09 19:07:47 -0300

    testando multiplos valores

--- 19-testes-automatizados/1-introducao/endereco/endereco_test.go
+++ 19-testes-automatizados/1-introducao/endereco/endereco_test.go
@@ -1,11 +1,34 @@
 // teste de unidade
 package endereco
 
-import "testing"
+import (
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+)
+
+// func TestTipoDeEndereco(t *testing.T) {
+// 	teste := TipoDeEndereco("Rua dos anjos")
+// 	if teste != "Rua" {
+// 		t.Errorf("Esperava %s e recebi %s", "Rua", teste)
+// 	}
+// }
+
+// teste multiplo
 func TestTipoDeEndereco(t *testing.T) {
-	teste := TipoDeEndereco("Rua dos anjos")
-	if teste != "Rua" {
-		t.Errorf("Esperava %s e recebi %s", "Rua", teste)
-	}
+	result := TipoDeEndereco("Rua 1")
+	assert.Equal(t, "Rua", result)
+
+	result = TipoDeEndereco("Alameda")
+	assert.Equal(t, "Alameda", result)
+
+	result = TipoDeEndereco("Avenida 1")
+	assert.Equal(t, "Avenida", result)
+
+	result = TipoDeEndereco("")
+	assert.Equal(t, "tipo inválido", result)
+
+	result = TipoDeEndereco("RUA 1")
+	assert.Equal(t, "Rua", result)
+
 }
