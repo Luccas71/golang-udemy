2dc2bd75a5464bf6a33cb0ca349df14e69f83c2e
Lucas Volpato <lmaciel916@gmail.com>
2024-06-09 10:22:19 -0300

    select

---
+++ 18-concorrencia/18.5-select/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//select => um switch para concorrencia*
+// quando tenho funções com execuções em tempos diferentes, posso usar o select para pular o tempo de espera da execução para chamar a outra função
+
+func main() {
+
+	canal1, canal2 := make(chan string), make(chan string)
+
+	go func() {
+		for {
+			time.Sleep(time.Millisecond * 500)
+			canal1 <- "Canal 1"
+		}
+	}()
+	go func() {
+		for {
+			time.Sleep(time.Second * 2)
+			canal2 <- "Canal 2"
+		}
+	}()
+
+	for {
+		select {
+		case mensagemCanal1 := <-canal1:
+			fmt.Println(mensagemCanal1)
+		case mensagemCanal2 := <-canal2:
+			fmt.Println(mensagemCanal2)
+		}
+	}
+}
