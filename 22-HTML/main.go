088fe9afc635bbdcce983b2421d98d1c2e166fb5
Lucas Volpato <lmaciel916@gmail.com>
2024-06-10 19:22:17 +0000

    servindo html dinâmico

--- 22-HTML/main.go
+++ 22-HTML/main.go
@@ -7,13 +7,23 @@ import (
 	"text/template"
 )
 
+type usuario struct {
+	Nome  string
+	Email string
+}
+
 var templates *template.Template
 
 func main() {
 	templates = template.Must(template.ParseGlob("*.html"))
 
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		templates.ExecuteTemplate(w, "index.html", nil)
+
+		u := usuario{
+			Nome:  "Lucas",
+			Email: "lucas@email.com",
+		}
+		templates.ExecuteTemplate(w, "index.html", u)
 	})
 	fmt.Println("Rodando na porta 8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
